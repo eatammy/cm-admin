f32b6506ebf69515e46921dd1269bb4ed1c99ddb
guoxuhui <simagle@163.com>
2016-04-13 09:32:30 +0800

    新增jquery1.x兼容库

--- works/js/index.js
+++ works/js/index.js
@@ -1,7 +1,25 @@
 /**
  * Created by simagle on 2016/4/12.
  */
+
+/*ajax内容加载*/
+jQuery(function ($) {
+    if ('enable_ajax_content' in ace) {
+        var options = {
+            content_url: function (url) {
+                if (url.indexOf("login") >= 0) {
+                    return false;
+                }
+                return "/html" + url;
+            },
+            default_url: '/welcome.html',
+            loading_icon: "fa-spinner fa-2x blue"
+        }
+        ace.enable_ajax_content($, options)
+    }
+})
 var CMADMIN = avalon.define({
     $id: "CMADMIN",
 });
+
 avalon.scan();
