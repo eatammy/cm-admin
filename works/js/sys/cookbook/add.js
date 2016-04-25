29a608bdf6e42ed91d22d9bd40dd025ade633239
simagle <simagle@163.com>
2016-04-25 23:49:03 +0800

    欢迎页面添加图片

--- works/js/sys/cookbook/add.js
+++ works/js/sys/cookbook/add.js
@@ -6,65 +6,66 @@ var uploadToken = "";
 $(function () {
     //上传
     uploader = Qiniu.uploader({
-        runtimes : 'html5,flash,html4', // 上传模式,依次退化
-        browse_button : 'pickFiles', // 上传选择的点选按钮，**必需**
+        runtimes: 'html5,flash,html4', // 上传模式,依次退化
+        browse_button: 'pickFiles', // 上传选择的点选按钮，**必需**
         // 在初始化时，uptoken, uptoken_url, uptoken_func 三个参数中必须有一个被设置
         // 切如果提供了多个，其优先级为 uptoken > uptoken_url > uptoken_func
         // 其中 uptoken 是直接提供上传凭证，uptoken_url 是提供了获取上传凭证的地址，如果需要定制获取 uptoken 的过程则可以设置 uptoken_func
         // uptoken : uploadToken, // uptoken 是上传凭证，由其他程序生成
         //uptoken_url : '/cm/admin/common/generalUploadToken', // Ajax 请求 uptoken 的 Url，**强烈建议设置**（服务端提供）
-         uptoken_func: function(file){    // 在需要获取 uptoken 时，该方法会被调用
+        uptoken_func: function (file) {    // 在需要获取 uptoken 时，该方法会被调用
             var uptoken = "";
-             $.ajax({
-                url:"/cm/admin/common/generalUploadToken",
+            $.ajax({
+                url: "/cm/admin/common/generalUploadToken",
                 dataType: "json",
                 type: "get",
+                async: false,
                 success: function (result) {
-                    if (isSuccess(result)){
+                    if (isSuccess(result)) {
                         uptoken = result.bizData;
                         avalon.log(uptoken);
-                    }else{
-                        layer.alert("获取上传凭证失败",2);
+                    } else {
+                        layer.alert("获取上传凭证失败", 2);
                     }
                 }
             });
             return uptoken;
-         },
-        get_new_uptoken : false, // 设置上传文件的时候是否每次都重新获取新的 uptoken
+        },
+        get_new_uptoken: false, // 设置上传文件的时候是否每次都重新获取新的 uptoken
         // downtoken_url: '/downtoken',
         // Ajax请求downToken的Url，私有空间时使用,JS-SDK 将向该地址POST文件的key和domain,服务端返回的JSON必须包含`url`字段，`url`值为该文件的下载地址
-        unique_names : true, // 默认 false，key 为文件名。若开启该选项，JS-SDK 会为每个文件自动生成key（文件名）
+        unique_names: true, // 默认 false，key 为文件名。若开启该选项，JS-SDK 会为每个文件自动生成key（文件名）
         // save_key: true,                  // 默认 false。若在服务端生成 uptoken 的上传策略中指定了 `sava_key`，则开启，SDK在前端将不对key进行任何处理
-        domain : 'http://7xnnot.com1.z0.glb.clouddn.com/', // bucket 域名，下载资源时用到，**必需**
-        container : 'headIcon', // 上传区域 DOM ID，默认是 browser_button 的父元素，
-        max_file_size : '100mb', // 最大文件体积限制
-        flash_swf_url : 'libs/upload/plupload/Moxie.swf', //引入 flash,相对路径
-        max_retries : 3, // 上传失败最大重试次数
-        dragdrop : true, // 开启可拖曳上传
-        drop_element : 'headIcon', // 拖曳上传区域元素的 ID，拖曳文件或文件夹后可触发上传
-        chunk_size : '4mb', // 分块上传时，每块的体积
-        auto_start : false, // 选择文件后自动上传，若关闭需要自己绑定事件触发上传,
-        filters : [ {
-            title : "Image files",
-            extensions : "jpg,gif,png"
-        } ],
-        init : {
-            'FilesAdded' : function(up, files) {
-                plupload.each(files, function(file) {
+        domain: 'http://7xnnot.com1.z0.glb.clouddn.com/', // bucket 域名，下载资源时用到，**必需**
+        container: 'headIcon', // 上传区域 DOM ID，默认是 browser_button 的父元素，
+        max_file_size: '100mb', // 最大文件体积限制
+        flash_swf_url: 'libs/upload/plupload/Moxie.swf', //引入 flash,相对路径
+        max_retries: 3, // 上传失败最大重试次数
+        dragdrop: true, // 开启可拖曳上传
+        drop_element: 'headIcon', // 拖曳上传区域元素的 ID，拖曳文件或文件夹后可触发上传
+        chunk_size: '4mb', // 分块上传时，每块的体积
+        auto_start: false, // 选择文件后自动上传，若关闭需要自己绑定事件触发上传,
+        filters: [{
+            title: "Image files",
+            extensions: "jpg,gif,png"
+        }],
+        init: {
+            'FilesAdded': function (up, files) {
+                plupload.each(files, function (file) {
                     // 文件添加进队列后,处理相关的事情
                     previewImage(file, function (imgsrc) {
-                        $("#img").attr("src",imgsrc);
+                        $("#img").attr("src", imgsrc);
                     })
                 });
             },
-            'BeforeUpload' : function(up, file) {
+            'BeforeUpload': function (up, file) {
                 // 每个文件上传前,处理相关的事情
             },
-            'UploadProgress' : function(up, file) {
+            'UploadProgress': function (up, file) {
                 // 每个文件上传时,处理相关的事情
                 console.log(file.percent)
             },
-            'FileUploaded' : function(up, file, info) {
+            'FileUploaded': function (up, file, info) {
                 // 每个文件上传成功后,处理相关的事情
                 // 其中 info 是文件上传成功后，服务端返回的json，形式如
                 // {
@@ -80,13 +81,13 @@ $(function () {
                     sourceLink);
 
             },
-            'Error' : function(up, err, errTip) {
+            'Error': function (up, err, errTip) {
                 //上传出错时,处理相关的事情
             },
-            'UploadComplete' : function() {
+            'UploadComplete': function () {
                 //队列文件处理完毕后,处理相关的事情
             },
-            'Key' : function(up, file) {
+            'Key': function (up, file) {
                 // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
                 // 该配置必须要在 unique_names: false , save_key: false 时才生效
 
@@ -115,7 +116,7 @@ $(function () {
             var preloader = new mOxie.Image();
             preloader.onload = function () {
                 //preloader.downsize(550, 400);//先压缩一下要预览的图片,宽300，高300
-                var imgsrc = preloader.type == 'image/jpeg'? preloader.getAsDataURL('image/jpeg', 80) : preloader.getAsDataURL(); //得到图片src,实质为一个base64编码的数据
+                var imgsrc = preloader.type == 'image/jpeg' ? preloader.getAsDataURL('image/jpeg', 80) : preloader.getAsDataURL(); //得到图片src,实质为一个base64编码的数据
                 callback && callback(imgsrc); //callback传入的参数为预览图片的url
                 preloader.destroy();
                 preloader = null;
@@ -123,6 +124,7 @@ $(function () {
             preloader.load(file.getSource());
         }
     }
+
     //表单校验
     var validator = $("#addForm").validate({
         rules: {
