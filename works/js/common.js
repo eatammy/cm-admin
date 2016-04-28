6da5de3ff5b004980e8dbe583b499e84ea150086
simagle <simagle@163.com>
2016-04-28 23:02:30 +0800

    添加guid生成方法

--- works/js/common.js
+++ works/js/common.js
@@ -5,6 +5,14 @@
 /** 分类**/
 var categoryType = ["食谱", "商店", "商品", "活动"];
 
+/** 产生guid **/
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+};
+function guid() {
+    return (S4()+S4()+S4()+S4()+S4()+S4()+S4()+S4());
+};
+
 /** 判断是否登录 **/
 function isLoad(){
     return sessionStorage.getItem("ISLOAD");
