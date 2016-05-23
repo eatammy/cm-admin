9ca461ef4cec48429cb545ca3d7c38d2c9d0d312
simagle <simagle@163.com>
2016-05-23 23:02:08 +0800

    最新活动模块呈现，新增商城活动模块

--- works/js/business/activity/add.js
+++ works/js/business/activity/add.js
@@ -4,20 +4,20 @@
 $(function () {
     //初始化时间选择器
     var start = {
-        elem: '#startTime',
-        format: 'YYYY/MM/DD hh:mm:ss',
+        elem: '#start',
+        format: 'YYYY-MM-DD hh:mm:ss',
         min: laydate.now(), //设定最小日期为当前日期
         max: '2099-06-16 23:59:59', //最大日期
         istime: true,
         istoday: false,
         choose: function(datas){
             end.min = datas; //开始日选好后，重置结束日的最小日期
-            end.start = datas //将结束日的初始值设定为开始日
+            end.start = datas; //将结束日的初始值设定为开始日
         }
     };
     var end = {
-        elem: '#endTime',
-        format: 'YYYY/MM/DD hh:mm:ss',
+        elem: '#end',
+        format: 'YYYY-MM-DD hh:mm:ss',
         min: laydate.now(),
         max: '2099-06-16 23:59:59',
         istime: true,
